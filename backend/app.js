b7c0dcb46fe3cdd73d107f68f87d76f5c32320dc
Vigneshwaran J <sjvignesh333@gmail.com>
2023-08-04 17:05:53 +0530

    Clearing Bugs

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,14 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/user',router);
 app.use('/api/blog',blogRouter);
 
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://admin:Vignesh3nec@cluster0.x9cdzjy.mongodb.net/BlogIn?retryWrites=true&w=majority&&ssl=true")
-.then(()=> app.listen(5000)).then(()=> console.log("Mongoose mandaiyan connected"))
+mongoose.connect("mongodb+srv://sjvignesh333:Vignesh3nec@cluster0.8l52yyp.mongodb.net/?retryWrites=true&w=majority")
+.then(()=> app.listen(5000)).then(()=> console.log("Mongoose connected"))
 .catch((err)=> console.log(err));
-
 
 
